40dbd8aa357bdc18319f4c3324517097ea2b6c9c
Limitex <76650151+Limitex@users.noreply.github.com>
2021-09-12 15:51:55 +0900

    first commit

---
+++ _mysql.php
@@ -0,0 +1,115 @@
+<?php
+class MySQL {
+    
+    private $MySQL_PDO, $SQL;
+
+    function __construct($hostname, $username, $password, $database) {
+        try {
+            $this->MySQL_PDO = new PDO ("mysql:host=$hostname;dbname=$database;charset=utf8", $username, $password);
+        } catch (PDOException $e) {
+            echo $e->getMessage;
+            exit;
+        }
+        $this->SQL = array(
+            'TABLES' => array(),
+            'COLUMN' => array(),
+            'CREATE' => array(), 
+            'INSERT' => array(),
+            'DELETE' => array(), 
+            'DROP' => array()
+        );
+    }
+
+    function __destruct() {
+        $this->MySQL_PDO = NULL;
+        $this->SQL = NULL;
+    }
+    
+    function send_sql($SQL, $parms = NULL) {
+        $stmt = $this->MySQL_PDO->prepare($SQL);
+        if($stmt->execute($parms)) return array( true, $stmt->fetchAll() );
+        else return array( false, 'SQL could not be executed for some reason.' );
+    }
+
+    function table_initialize($table, $array) {
+        if(in_array($table, $this->SQL['TABLES'])) return array( false, 'The table is already defined.');
+        $array = array('ID' => "int AUTO_INCREMENT NOT NULL PRIMARY KEY") + $array;
+        $create = ''; $insert = array('', ''); $column = array();
+        foreach($array as $key => $value){
+            $create = $create.$key.' '.$value.', ';
+            if($key != 'ID'){
+                $insert[0] = $insert[0].$key.', ';
+                $insert[1] = $insert[1].':'.$key.', ';
+                $column += array(count($column) => $key);
+            }
+        }
+        // $this->SQL['CREATE'][$table] = "CREATE TABLE IF NOT EXISTS `$table` ".'('.rtrim($create, ', ').');';
+        $this->SQL['TABLES'][count($this->SQL['TABLES'])] = $table;
+        $this->SQL['COLUMN'][$table] = $column;
+        $this->SQL['CREATE'][$table] = "CREATE TABLE `$table` ".'('.rtrim($create, ', ').');';
+        $this->SQL['INSERT'][$table] = "INSERT INTO `$table` ".'('.rtrim($insert[0], ', ').') VALUES ('.rtrim($insert[1], ', ').');';
+        $this->SQL['DELETE'][$table] = "DELETE FROM `$table` WHERE ID = :ID; SET @i := 0; UPDATE `$table` SET ID = (@i := @i + 1);";
+        $this->SQL['DROP'][$table] = "DROP TABLE `$table`;";
+        return array( true, '' );
+    }
+
+    function show_sqls() {
+        foreach($this->SQL as $key1 => $array){
+            foreach($array as $key => $value){
+                if(!is_array($value)){
+                    echo '['.$key1.']['.$key.'] = "'.$value.'"<br>';
+                }else{
+                    foreach($value as $k => $a){
+                        echo '['.$key1.']['.$key.']['.$k.'] = "'.$a.'"<br>';
+                    }
+                }
+                
+            }
+            echo "<br>";
+        }
+    }
+
+    function CREATE($table = NULL) {
+        if(empty($table)){
+            $count = 0;
+            foreach ($this->SQL['CREATE'] as $sql){
+                $result = $this->send_sql($sql);
+                if(!$result[0]) $count++;
+            }
+            return array( $count == 0, $count, $count.' failures' );
+        }else{
+            foreach ($this->SQL['CREATE'] as $key => $sql){
+                if($table == $key){
+                    $result = $this->send_sql($sql);
+                    if(!$result[0]) return array( false, 1, '1 failures' );
+                    return array( true, 0, '0 failures' );
+                }
+            }
+            return array( false, -1, 'The specified table is not defined.');
+        }
+    }
+
+    function INSERT($table, $array) {
+        $sql_columns = $this->SQL['COLUMN'][$table];
+        $insert_columns = array_keys($array);
+        $params = array();
+        foreach($sql_columns as $sc){
+            foreach($insert_columns as $ic){
+                if($sc == $ic){
+                    $params += array(':'.$sc => $array[$ic]);
+                }
+            }
+        }
+        $this->send_sql($this->SQL['INSERT'][$table], $params);
+    }
+
+    function DELETE($table, $id) {
+        $this->send_sql($this->SQL['DELETE'][$table], array(':ID' => $id));
+    }
+
+    function DROP($table) {
+        $this->send_sql($this->SQL['DROP'][$table]);
+        unset($this->SQL['DROP'][$table]);
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    require( $_SERVER['DOCUMENT_ROOT'] . '/_mysql.php');
+
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '0000';
+    $database = 'test_database';
+
+    $table_name1 = 'user_list';
+    $table_name2 = 'list';
+    $table_name3 = 'setting';
+
+    $table_init1 = array('UserName' => 'varchar(255)', 'PasswordHash' => 'varchar(511)', 'Position' => 'varchar(255)');
+    $table_init2 = array('UserName' => 'varchar(255)', 'CreatorIP' => 'varchar(15)', 'CreatedTime' => 'datetime', 'Description' => 'text(511)');
+    $table_init3 = array('Item' => 'varchar(255)', 'Value' => 'varchar(511)');
+
+    $mysql = new MySQL($hostname,$username,$password,$database);
+    
+    $mysql->table_initialize($table_name1, $table_init1);
+    $mysql->table_initialize($table_name2, $table_init2);
+    $mysql->table_initialize($table_name3, $table_init3);
+
+    $mysql->show_sqls();
+
+    $mysql->CREATE();
+
+    $mysql->INSERT($table_name1, array('UserName' => 'val_username', 'PasswordHash' => 'val_passwordhash', 'Position' => 'val_position'));
+    $mysql->INSERT($table_name2, array('UserName' => 'val_username', 'CreatorIP' => 'val_creatorIp', 'CreatedTime' => 'val_createdtime', 'Description' => 'val_description'));
+    $mysql->INSERT($table_name3, array('Item' => 'val_item', 'Value' => 'val_value'));
+
+    // $mysql->DELETE($table_name1, 2);
+    // $mysql->DELETE($table_name2, 2);
+    // $mysql->DELETE($table_name3, 2);
+
+    // $mysql->DROP($table_name1);
+    // $mysql->DROP($table_name2);
+    // $mysql->DROP($table_name3);
+?>
